2760ba8d3cedde50ce210375f23b83439c52e9b0
Miram Atie <Miram@dyn1187-86.wlan.ic.ac.uk>
2016-10-10 09:28:45 +0100

    Added Data Set Information, proportions, regression on categorical predictors

--- student-merge.R
+++ student-merge.R
@@ -3,21 +3,36 @@
 #- Correlation analyses
 #- Regression
 #- _Above all_: you should tell me something interesting about what is going on in the data.
+
+# MA: Add Data Set Information
+# Source: Paulo Cortez, University of Minho, GuimarÃ£es, Portugal, http://www3.dsi.uminho.pt/pcortez
+# This data approach student achievement in secondary education of two Portuguese schools. 
+# The data attributes include student grades, demographic, social and school related features). 
+# The data was collected by using school reports and questionnaires. 
+# Two datasets are provided regarding the performance in two distinct subjects: Mathematics (mat) and Portuguese language (por). 
+# The two datasets were modeled under binary/five-level classification and regression tasks.
+
 library(ggplot2)
-d1=read.csv("student-mat.csv",header=TRUE)
-d2=read.table("student-por.csv",sep=";",header=TRUE)
+# MA: Change variable names
+mat_ds <- read.csv("data/student-mat.csv", header=TRUE)
+por_ds <- read.table("data/student-por.csv", sep=";", header=TRUE)
 
-d1
-d2
+# MA: Print first 10 rows of each data set
+head(mat_ds, n = 10)
+head(por_ds, n = 10)
 
-d3=rbind(d1,d2)
-print(nrow(d3)) # 1044 students
-d3
+student_ds <- rbind(mat_ds, por_ds)
+print(nrow(student_ds)) # 1044 students
+
+head(student_ds, n = 10)
+# MA: Description of variables in the final data set
+str(student_ds)
+
 #uhh guys there's 386 students in both datasets so the rbind didn't exactly work
 # to do any analysis, we should convert rows to numeric as opposed to binary/categorical
 # nvm the regression can automatically convert categorical so we good fam.
 
-#The combined student dataset contains 32 variables
+# The combined student dataset contains 32 variables
 # School      | Student's school (binary: "GP" - Gabriel Pereira or "MS" - Mousinho da Silveira)  #need to convert to 1/0
 # Sex         | Student's sex (binary: "F" - female or "M" - male)                                #need to convert to 1/0
 # Age         | Student's age (numeric: from 15 to 22)
@@ -55,15 +70,34 @@ d3
 #32 G3 - final grade (numeric: from 0 to 20, output target)
 
 # univariate statistics. we can use bar plots to do some bivariate analysis
-summary(d3)
-str(d3)
+summary(student_ds)
+# MA: Some basic analysis on the data
+# Most of the students in this data set study at Gabriel Pereira (772 out of 1044)
+# Female Percent
+f_pct <- paste0(nrow(student_ds[student_ds[,"sex"] == "F",]) / nrow(student_ds) * 100, "%")
+# 56.6% female students
+# Male Percent
+m_pct <- paste0(nrow(student_ds[student_ds[,"sex"] == "M",]) / nrow(student_ds) * 100, "%")
+# 43.4% male students
+# Average grade of female students
+f_grade <- mean(student_ds[student_ds[,"sex"] == "F", "G3"])
+# 11.44839
+# Average grade of male students
+m_grade <- mean(student_ds[student_ds[,"sex"] == "M", "G3"])
+# 11.20309
+# Difference between female and male proportions with respect to certain characteristics
+prop.test(table(student_ds$activities, student_ds$sex), correct = FALSE)
+prop.test(table(student_ds$nursery, student_ds$sex), correct = FALSE)
+prop.test(table(student_ds$higher, student_ds$sex), correct = FALSE)
+prop.test(table(student_ds$internet, student_ds$sex), correct = FALSE)
+prop.test(table(student_ds$romantic, student_ds$sex), correct = FALSE)
 
 # barplots for scores based on various characteristics is the general idea. 
 # forgetting how to do it where instead of count, the bar graph shows the mean based on various characterists
 # gaaaah train has no internet....
-ggplot(d3,aes(x = school, fill = G3)) + geom_bar()
+ggplot(student_ds, aes(x = school, fill = G3)) + geom_bar()
 
-pairs(d3)
+pairs(student_ds)
 pairs
 
 #what's correlated with each other??? how to correlate categorical variables?
@@ -73,9 +107,70 @@ pairs
 
 
 #giant linear model of what best predicts G3 score?
-model1 <- lm(d3$G3 ~ d3$address + d3$school + d3$romantic + d3$famsize + d3$Medu)
+model1 <- lm(student_ds$G3 ~ student_ds$address + student_ds$school + student_ds$romantic + student_ds$famsize + student_ds$Medu)
 summary(model1)
 # questions to ask. does mom or dad's education status and/or job affect scores more
 # looks like having a romantic interest distracts students from studying
 # also looks like urban students do better than rural ones
 # one of the schools does way better than the other? can we find out how much of that school variation is due to the school vs other characteristics
+
+# MA model
+# Negative relationships
+summary(lm(formula = student_ds$G3 ~ student_ds$health, data = student_ds))
+# p-value: 0.00964 , slope: -0.21723
+summary(lm(formula = student_ds$G3 ~ student_ds$Walc, data = student_ds))
+# p-value: 0.000178 , slope: -0.34807
+summary(lm(formula = student_ds$G3 ~ student_ds$Dalc, data = student_ds))
+# p-value: 2.65e-05 , slope: -0.5496
+summary(lm(formula = student_ds$G3 ~ student_ds$goout, data = student_ds))
+# p-value: 0.00154 , slope: -0.3282
+summary(lm(formula = student_ds$G3 ~ student_ds$freetime, data = student_ds))
+# p-value: 0.0361 , slope: -0.2431
+summary(lm(formula = student_ds$G3 ~ student_ds$romantic, data = student_ds))
+# p-value: 0.00146 , slope: -0.7939
+summary(lm(formula = student_ds$G3 ~ student_ds$traveltime, data = student_ds))
+# p-value: 0.000898 , slope: -0.5421
+
+# Positive relationships
+summary(lm(formula = student_ds$G3 ~ student_ds$internet, data = student_ds))
+# p-value: 0.0005299 , slope: 1.0192
+summary(lm(formula = student_ds$G3 ~ student_ds$higher, data = student_ds))
+# p-value: 9.55e-15 , slope: 3.2726
+summary(lm(formula = student_ds$G3 ~ student_ds$studytime, data = student_ds))
+# p-value: 1.52e-07 , slope: 0.7487
+summary(lm(formula = student_ds$G3 ~ student_ds$Fjob, data = student_ds))
+# p-value: 0.007 , slope: 1.8454 (Fjobteacher)
+summary(lm(formula = student_ds$G3 ~ student_ds$Mjob, data = student_ds))
+# p-value: 1.16e-05 , slope: 2.2139 (Mjobhealth)
+# p-value: 0.00126 , slope: 1.1920 (Mjobservices)
+# p-value: 6.21e-05 , slope: 1.7386 (Mjobteacher)
+summary(lm(formula = student_ds$G3 ~ student_ds$Fedu, data = student_ds))
+# p-value: 2.1e-07 , slope: 0.5615
+summary(lm(formula = student_ds$G3 ~ student_ds$Medu, data = student_ds))
+# p-value: 5.05e-11 , slope: 0.6922
+
+# should we convert categorical variables to factors?
+mod2 <- lm(formula = student_ds$G3 ~ student_ds$higher + student_ds$studytime + 
+             student_ds$Mjob + student_ds$Fedu + student_ds$Medu + student_ds$Walc + 
+             student_ds$Dalc + student_ds$traveltime, data = student_ds)
+summary(mod2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
